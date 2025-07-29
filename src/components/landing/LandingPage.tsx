487f68f49831bd1d2bc94e77265aeb3281749f88
ConstructIA_Dev <constructiapruebas@gmail.com>
2025-07-29 14:40:43 +0200

    Corregir cierre de etiquetas JSX

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -868,6 +868,12 @@ export default function LandingPage() {
                 <Settings className="h-5 w-5" />
               </Link>
             </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
 
 export default LandingPage
   )
