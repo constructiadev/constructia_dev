cbfc0ca0fb1dd2a7bee762b951cffe2bb89445d9
ConstructIA_Dev <constructiapruebas@gmail.com>
2025-08-01 14:51:06 +0200

    Fix Chart.js setup and configuration

---
+++ src/lib/chartSetup.ts
@@ -0,0 +1,29 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  Filler
+} from 'chart.js';
+
+// Register Chart.js components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  Filler
+);
+
+export default ChartJS;
