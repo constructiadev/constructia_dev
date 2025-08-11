05576e9b519ff68dbdb71e1f37976cc002790594
ConstructIA_Dev <constructiapruebas@gmail.com>
2025-08-11 10:49:47 +0200

    Eliminar configuración CORS

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    cors: {
-      origin: false,
-      credentials: false
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
