6a03a9eabb2bfb0491384e5f1906aa0e2f74accc
ConstructIA_Dev <constructiapruebas@gmail.com>
2025-09-10 11:16:17 +0200

    Fix AuthProvider scope by moving it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Router from './components/Router';
 import CookieConsent from './components/common/CookieConsent';
+import { AuthProvider } from './lib/auth-context';
 
 function App() {
   console.log('🔍 [App] Component rendering - Development Mode');
   
   return (
-    <>
-      <Router />
-      <CookieConsent />
-    </>
+    <AuthProvider>
+      <>
+        <Router />
+        <CookieConsent />
+      </>
+    </AuthProvider>
   );
 }
 
