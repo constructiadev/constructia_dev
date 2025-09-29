aff9151dc5e9162baaae9f3e71171160043a696b
ConstructIA_Dev <constructiapruebas@gmail.com>
2025-09-29 09:28:21 +0200

    Fix duplicate button closing tag

--- src/components/client/PlatformCredentialsManager.tsx
+++ src/components/client/PlatformCredentialsManager.tsx
@@ -274,7 +274,6 @@ export default function PlatformCredentialsManager({
                 )}
               </div>
             </button>
-            </button>
           );
         })}
       </div>
